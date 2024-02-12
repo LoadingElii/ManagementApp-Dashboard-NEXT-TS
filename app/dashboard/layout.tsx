2c0f0df808b55642fb87a5a9957f71d385e5ac48
LoadingElii <delijhiabrown@gmail.comgit config --global user.nam delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.comgit config --global user.email delijhiabrown@gmail.com>
2024-02-11 22:38:01 -0600

    Fixed Hydration bug

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import Navbar from "../ui/dashboard/nav-bar";
 
 export default function Layout({ children } : { children : React.ReactNode }){
     return (
-      <html className=" h-full overflow-hidden">
+      <div className=" h-full overflow-hidden">
         <div className=" shadow-md ">
           <Navbar />
         </div>
         <div className=" bg-gray-300 h-full "> {children} </div>
-      </html>
+      </div>
     );
 }
